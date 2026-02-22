fc188e3e1e2fc42c92fc5b5287850ba0d494825d
karimngami13 <karimngami13@gmail.com>
2026-02-22 14:45:56 -0500

    Initial setup - Angular + Firebase + AngularFire + Emulators config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,61 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth, connectAuthEmulator } from '@angular/fire/auth';
+import { provideFirestore, getFirestore, connectFirestoreEmulator } from '@angular/fire/firestore';
+import { provideFunctions, getFunctions, connectFunctionsEmulator } from '@angular/fire/functions';
+import { provideStorage, getStorage, connectStorageEmulator } from '@angular/fire/storage';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBl3aRO-eDsM5iALRj-Rt2bpzKOeqYBLQ0",
+  authDomain: "todo-manager-prod.firebaseapp.com",
+  projectId: "todo-manager-prod",
+  storageBucket: "todo-manager-prod.firebasestorage.app",
+  messagingSenderId: "108238241356",
+  appId: "1:108238241356:web:f1d3b0e5e4c02592643ee9"
+};
+
+const useEmulators = true; // ← false quand tu déploies en prod
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+
+    provideAuth(() => {
+      const auth = getAuth();
+      if (useEmulators) {
+        connectAuthEmulator(auth, 'http://127.0.0.1:9099', { disableWarnings: true });
+      }
+      return auth;
+    }),
+
+    provideFirestore(() => {
+      const firestore = getFirestore();
+      if (useEmulators) {
+        connectFirestoreEmulator(firestore, '127.0.0.1', 8080);
+      }
+      return firestore;
+    }),
+
+    provideFunctions(() => {
+      const functions = getFunctions();
+      if (useEmulators) {
+        connectFunctionsEmulator(functions, '127.0.0.1', 5001);
+      }
+      return functions;
+    }),
+
+    provideStorage(() => {
+      const storage = getStorage();
+      if (useEmulators) {
+        connectStorageEmulator(storage, '127.0.0.1', 9199);
+      }
+      return storage;
+    }),
+  ]
 };
